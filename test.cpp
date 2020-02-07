97a68da533f4f6158c0a547d21a0228d87120f69
happyPenguinMac <hevin@Hannahs-MacBook-Pro.local>
2020-02-06 22:35:21 -0700

    testing. edited to get code to run

--- test.cpp
+++ test.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int add(int, int);
+
+int main() {
    cout << "Hello World!\n";
    cout << " Whatever (said the world)\n\n";
    cout << add(12,29);
-   cout << " ewwww lala !!!!\n\n"
-   int dees-balls = 52;
-   cout << dees-balls << "\n\n";
+   cout << " ewwww lala !!!!\n\n";
+   int deesballs = 52;
+   cout << deesballs << "\n\n";
    return 0;
 }
-// this needs to be declared above main to work maybe...... h aha ha ha ahah aha hah ah ah ha hah a 
-int add(int a,int b)
-{
+
+// this needs to be declared above main to work maybe...... h aha ha ha ahah aha hah ah ah ha hah a
+// Fixed!
+int add(int a,int b) {
 	return a+b;
 }
