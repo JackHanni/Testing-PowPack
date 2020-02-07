ee09bc24fe49425dffc8dc17adab0cf682434359
Iron <adencjessee@gmail.com>
2020-02-06 23:57:25 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 int add(int, int);
 
-int main() {
+int main() { 
    cout << "Hello World!\n";
    cout << " Whatever (said the world)\n\n";
    cout << add(12,29);
    cout << " ewwww lala !!!!\n\n";
    int deesballs = 52;
    cout << deesballs << "\n\n";
-
    return 0;
 }
 
 // Fixed!
-// I like it
+// I like it 
 int add(int a, int b) {
-	return a+b;
+   return a+b;
 }
