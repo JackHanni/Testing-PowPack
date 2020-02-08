0a769cf7210a768c7ea7db71a955ec4c98f6924d
JackHanni <jhanni@gci.net>
2020-02-07 19:13:33 -0700

    some classy spheres

---
+++ spheres/sphere.h
@@ -0,0 +1,26 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include <iostream>
+
+struct Point
+{
+   float x;
+   float y;
+   float z;
+};
+
+class Sphere
+{
+   private:
+      float radius;
+      static float density;
+      static float totalMass;
+      static int sphereCount;
+      Point p;
+      float mass;
+   public:
+      float calculateVolume();
+};
+
+#endif
